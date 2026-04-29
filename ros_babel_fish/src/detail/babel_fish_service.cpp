9b18ed00d036947978e08e68dbd4c6b51c2461c3
Scott K Logan <logans@cottsay.net>
2026-04-29 15:11:14 -0500

    Revert "Trimmed the branch to only the ros_babel_fish_test_msgs sub directory"
    This reverts commit e79c5677a07a6277fcfb0a791fa481b9059d87e3.

---
+++ ros_babel_fish/src/detail/babel_fish_service.cpp
@@ -0,0 +1,106 @@
+// Copyright (c) 2021 Stefan Fabian. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "ros_babel_fish/detail/babel_fish_service.hpp"
+#include "ros_babel_fish/idl/serialization.hpp"
+
+#include <rosidl_typesupport_introspection_cpp/service_introspection.hpp>
+
+namespace ros_babel_fish
+{
+
+BabelFishService::BabelFishService( std::shared_ptr<rcl_node_t> node_base,
+                                    const std::string &service_name,
+                                    ServiceTypeSupport::ConstSharedPtr type_support,
+                                    AnyServiceCallback callback, rcl_service_options_t options )
+    : ServiceBase( std::move( node_base ) ), type_support_( std::move( type_support ) ),
+      callback_( std::move( callback ) )
+{
+  // rcl does the static memory allocation here
+  service_handle_ = std::shared_ptr<rcl_service_t>( new rcl_service_t, [handle = node_handle_](
+                                                                           rcl_service_t *service ) {
+    if ( rcl_service_fini( service, handle.get() ) != RCL_RET_OK ) {
+      RCLCPP_ERROR( rclcpp::get_node_logger( handle.get() ).get_child( "rclcpp" ),
+                    "Error in destruction of rcl service handle: %s", rcl_get_error_string().str );
+      rcl_reset_error();
+    }
+    delete service;
+  } );
+  *service_handle_ = rcl_get_zero_initialized_service();
+
+  rcl_ret_t ret =
+      rcl_service_init( service_handle_.get(), node_handle_.get(),
+                        &type_support_->type_support_handle, service_name.c_str(), &options );
+  if ( ret != RCL_RET_OK ) {
+    if ( ret == RCL_RET_SERVICE_NAME_INVALID ) {
+      auto rcl_node_handle = get_rcl_node_handle();
+      // this will throw on any validation problem
+      rcl_reset_error();
+      rclcpp::expand_topic_or_service_name( service_name, rcl_node_get_name( rcl_node_handle ),
+                                            rcl_node_get_namespace( rcl_node_handle ), true );
+    }
+
+    rclcpp::exceptions::throw_from_rcl_error( ret, "could not create service" );
+  }
+  TRACETOOLS_TRACEPOINT( rclcpp_service_callback_added,
+                         static_cast<const void *>( get_service_handle().get() ),
+                         static_cast<const void *>( &callback_ ) );
+#ifndef TRACETOOLS_DISABLED
+  callback_.register_callback_for_tracing();
+#endif
+}
+
+bool BabelFishService::take_request( CompoundMessage &request_out, rmw_request_id_t &request_id_out )
+{
+  std::shared_ptr<void> type_erased = create_request();
+  if ( !take_type_erased_request( type_erased.get(), request_id_out ) )
+    return false;
+  request_out = CompoundMessage( type_support_->request(), std::move( type_erased ) );
+  return true;
+}
+
+void BabelFishService::send_response( rmw_request_id_t &request_id, CompoundMessage &response )
+{
+  rcl_ret_t ret = rcl_send_response( get_service_handle().get(), &request_id,
+                                     response.type_erased_message().get() );
+
+  if ( ret != RCL_RET_OK ) {
+    rclcpp::exceptions::throw_from_rcl_error( ret, "failed to send response" );
+  }
+}
+
+std::shared_ptr<void> BabelFishService::create_request()
+{
+  return createContainer( type_support_->request() );
+}
+
+std::shared_ptr<rmw_request_id_t> BabelFishService::create_request_header()
+{
+  return std::make_shared<rmw_request_id_t>();
+}
+
+void BabelFishService::handle_request( const std::shared_ptr<rmw_request_id_t> &request_header,
+                                       const std::shared_ptr<void> &request )
+{
+  auto typed_request = CompoundMessage::make_shared( type_support_->request(), request );
+  auto response = CompoundMessage::make_shared( type_support_->response() );
+  callback_.dispatch( this->shared_from_this(), request_header, typed_request, response );
+  send_response( *request_header, *response );
+}
+
+void BabelFishService::configure_introspection( const rclcpp::Clock::SharedPtr &clock,
+                                                const rclcpp::QoS &qos_service_event_pub,
+                                                rcl_service_introspection_state_t introspection_state )
+{
+  rcl_publisher_options_t pub_opts = rcl_publisher_get_default_options();
+  pub_opts.qos = qos_service_event_pub.get_rmw_qos_profile();
+
+  rcl_ret_t ret = rcl_service_configure_service_introspection(
+      service_handle_.get(), node_handle_.get(), clock->get_clock_handle(),
+      &type_support_->type_support_handle, pub_opts, introspection_state );
+
+  if ( RCL_RET_OK != ret ) {
+    rclcpp::exceptions::throw_from_rcl_error( ret, "failed to configure service introspection" );
+  }
+}
+} // namespace ros_babel_fish
